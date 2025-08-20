029b77b19558d33f2ab2f8d5308cccd482cf9ed2
alcino-luvualo <kenikenilson@gmail.com>
2025-08-20 15:24:57 +0100

    Função exibir livros na pagina criada

--- src/script/script.js
+++ src/script/script.js
@@ -56,3 +56,8 @@ function displaybook() {
 
   addEventListener();
 }
+
+addBookToLibrary("O Hobbit", "J.R.R. Tolkien", 310, true);
+addBookToLibrary("1984", "George Orwell", 328, false);
+addBookToLibrary("Dom Casmurro", "Machado de Assis", 256, true);
+addBookToLibrary("O Pequeno Príncipe", "Antoine de Saint-Exupéry", 96, false);
