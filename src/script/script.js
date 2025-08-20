5cea391a3235e2c3db0cb4839e883eb250e4cd1a
alcino-luvualo <kenikenilson@gmail.com>
2025-08-20 15:23:09 +0100

    Função exibir livros na pagina criada

--- src/script/script.js
+++ src/script/script.js
@@ -25,3 +25,34 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(novoLivro);
   displaybook();
 }
+
+function displaybook() {
+  livros.innerHtml = "";
+
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
+    div.classList.add("livro");
+    bookCard.setAttribute("data-id", book.id);
+
+    bookCard.innerHTML = `
+    <div class="book-info">
+      <h3>${book.title}</h3>
+      <p><strong>Author:</strong>${book.author}</p>
+      <p><strong>Paginas:</strong>${book.pages}</p>
+      <p><strong>Status:</strong>${book.read ? "Lido" : "Não Lido"}</p>
+    </div>
+    <div class ="book-actions">
+      <button class = "toggle-read-btn" data-id= "${book.id}">
+        ${book.read ? "Marcar como Não lido" : "Marcar como lido"}
+      </button>
+      <button class = "remove-book-btn" data-id = "${book.id}">
+        Remover Livro
+      </button>
+    </div>
+      `;
+
+      livros.appendChild(bookCard);
+  });
+
+  addEventListener();
+}
